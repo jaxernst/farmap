fb44e129e27c81de97f96861d5a74cfe1dd9ed9a
Jackson Ernst - jaxer.eth <51183683+jaxernst@users.noreply.github.com>
2025-04-07 18:35:51 -0700

    Map fixes

--- packages/ui/src/routes/+page.svelte
+++ packages/ui/src/routes/+page.svelte
@@ -48,7 +48,7 @@
 	<h1
 		class="transform font-['Comic_Neue'] text-3xl leading-5 font-black text-purple-500 transition-transform duration-300 hover:scale-110"
 	>
-		Photo Map
+		Far Map
 	</h1>
 	<p class="text-xs leading-5 font-medium text-neutral-400">Upload Everywhere</p>
 </div>
