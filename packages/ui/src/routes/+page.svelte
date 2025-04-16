3b9b55b45fae3601050d0b3f09721f8c60dda8ad
Jackson Ernst - jaxer.eth <51183683+jaxernst@users.noreply.github.com>
2025-04-16 16:15:52 -0700

    Open in farcaster

--- packages/ui/src/routes/+page.svelte
+++ packages/ui/src/routes/+page.svelte
@@ -24,3 +24,24 @@
 {#if userStore.user}
 	<ControlsOverlay />
 {/if}
+
+<!-- Display 'open in farcaster button overlay if there's no frame context -->
+{#await sdk.context then context}
+	{#if !context}
+		<div class="fixed inset-0 z-[1000] flex items-center justify-center">
+			<div class="rounded-3xl border-2 border-purple-400 bg-white">
+				<button
+					class="flex items-center gap-2 rounded-md px-4 py-2 text-lg font-medium text-purple-500"
+					onclick={() => {
+						window.open(
+							`https://warpcast.com/~/developers/mini-apps/preview?url=${window.location.href}`,
+							'_blank'
+						);
+					}}
+				>
+					Open app in Farcaster
+				</button>
+			</div>
+		</div>
+	{/if}
+{/await}
