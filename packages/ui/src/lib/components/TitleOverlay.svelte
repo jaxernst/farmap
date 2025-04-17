2cad74276bdb326e679403f41852abfdc7da3b8b
Jackson Ernst - jaxer.eth <51183683+jaxernst@users.noreply.github.com>
2025-04-16 22:16:37 -0700

    TItle padding

--- packages/ui/src/lib/components/TitleOverlay.svelte
+++ packages/ui/src/lib/components/TitleOverlay.svelte
@@ -1,11 +1,11 @@
-<div class="fixed top-2 right-2 z-[1000] flex flex-col items-center justify-center">
+<div class="fixed right-4 top-4 z-[1000] flex flex-col items-center justify-center">
 	<h1
-		class="transform font-['Architects_Daughter'] text-[33px] leading-5 font-bold tracking-tight text-purple-500 transition-transform duration-300 hover:scale-110"
+		class="transform font-['Architects_Daughter'] text-[33px] font-bold leading-5 tracking-tight text-purple-500 transition-transform duration-300 hover:scale-110"
 	>
 		Far Map
 	</h1>
 	<p
-		class=" font-['Architects_Daughter'] text-[11px] leading-5 font-medium tracking-tighter text-neutral-500"
+		class=" font-['Architects_Daughter'] text-[11px] font-medium leading-5 tracking-tighter text-neutral-500"
 	>
 		Upload Everywhere
 	</p>
