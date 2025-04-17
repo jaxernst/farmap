eb89df810f0a71e8c7ef0f8fae89b106bea890c3
Jackson Ernst - jaxer.eth <51183683+jaxernst@users.noreply.github.com>
2025-04-16 17:47:06 -0700

    Vite config fix

--- packages/ui/vite.config.ts
+++ packages/ui/vite.config.ts
@@ -1,19 +1,21 @@
+import { defineConfig, loadEnv } from 'vite';
 import tailwindcss from '@tailwindcss/vite';
 import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vite';
 
-const PUBLIC_API_URL = "http://localhost:3001"
+export default defineConfig(({ mode }) => {
+	const env = loadEnv(mode, process.cwd(), '');
 
-export default defineConfig({
-	plugins: [tailwindcss(), sveltekit()],
-	server: {
-		proxy: {
-			'/api': {
-				target: PUBLIC_API_URL,
-				changeOrigin: true,
-				rewrite: (path) => path.replace(/^\/api/, '')
-			}
-		},
-		allowedHosts: true
-	},
+	return {
+		plugins: [tailwindcss(), sveltekit()],
+		server: {
+			proxy: {
+				'/api': {
+					target: env.PUBLIC_API_URL,
+					changeOrigin: true,
+					rewrite: (path) => path.replace(/^\/api/, '')
+				}
+			},
+			allowedHosts: true
+		}
+	};
 });
