92be35005066e3d4bd7a0c188842b063ec4cfd1f
Jackson Ernst - jaxer.eth <51183683+jaxernst@users.noreply.github.com>
2025-04-16 21:57:20 -0700

    Revert "Try"
    This reverts commit 0252d35c05814a525459434020d130782cf38099.

--- packages/server/src/Sql.ts
+++ packages/server/src/Sql.ts
@@ -1,16 +1,7 @@
-import type { SqlClient } from "@effect/sql";
+import { Context, Layer } from "effect";
 import { SqliteClient, SqliteMigrator } from "@effect/sql-sqlite-node";
-import type { Context, Layer } from "effect";
-
-import * as fs from "fs";
-import * as path from "path";
+import { SqlClient } from "@effect/sql";
 import { fileURLToPath } from "url";
-
-// Make a db directory if one doesn't exist
-const dbDir = path.join(process.cwd(), "db");
-if (!fs.existsSync(dbDir)) {
-  fs.mkdirSync(dbDir, { recursive: true });
-}
 
 const sqliteClientLive = SqliteClient.layer({
   filename: "./db/db.dev.sql",
@@ -31,6 +22,6 @@ export class Db extends Context.Tag("InitializedDatabase")<
   Db,
   SqlClient.SqlClient
 >() {
-  static readonly SqliteLive = Layer.provideMerge(MigratorLive, sqliteClientLive)
-  static readonly Test = Layer.provideMerge(MigratorLive, sqliteClientTest)
+  static readonly SqliteLive = Layer.provideMerge(MigratorLive, sqliteClientLive);
+  static readonly Test = Layer.provideMerge(MigratorLive, sqliteClientTest);
 }
