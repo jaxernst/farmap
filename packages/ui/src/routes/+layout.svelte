926ed43eba7b9a060b82c6c60f302a09458bfc81
Jackson Ernst - jaxer.eth <51183683+jaxernst@users.noreply.github.com>
2025-04-21 13:00:08 -0700

    Rename

--- packages/ui/src/routes/+layout.svelte
+++ packages/ui/src/routes/+layout.svelte
@@ -2,27 +2,11 @@
 	import '../app.css';
 	import 'leaflet/dist/leaflet.css';
 	import { sdk } from '@farcaster/frame-sdk/src';
-	import { initializeApp } from '$lib/AppInit.svelte';
+	import { initializeApp } from '$lib/app-init';
 	import { page } from '$app/state';
 	import Map from '../lib/components/Map.svelte';
 	import { browser } from '$app/environment';
 	import { mapStore } from '../lib/Map.svelte';
-
-	/** Scratch notes
-	 Prod:
-	 	- CDN for S3
-
-  Map loading / initialization behavior: 
-		- Should auto zoom all the way out when not signed in and just show jaxer.eth attachments
-		
-		On map load:
-			if focus attachment, go there
-			else wait to be signed in 
-
-			if signed in: Go to user's last known location
-			else: Zoom all the way out, show all public user markers
-
-	 */
 
 	let { children } = $props();
 
