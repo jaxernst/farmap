f81cdf3b044ad8a4335f638b2fc34227ca9c875f
Jackson Ernst - jaxer.eth <51183683+jaxernst@users.noreply.github.com>
2025-04-23 16:19:49 -0700

    Logo

--- packages/ui/src/lib/components/TitleOverlay.svelte
+++ packages/ui/src/lib/components/TitleOverlay.svelte
@@ -1,11 +1,11 @@
 <div class="fixed right-4 top-4 z-[1000] flex flex-col items-center justify-center">
 	<h1
-		class="transform font-['Architects_Daughter'] text-[33px] font-bold leading-5 tracking-tight text-purple-500 transition-transform duration-300 hover:scale-110"
+		class="transform font-['Architects_Daughter'] text-[33px] font-bold leading-5 tracking-normal text-purple-500 transition-transform duration-300 hover:scale-110"
 	>
-		Far Map
+		Far<span class="text-[14px]">{' '}</span>Map
 	</h1>
 	<p
-		class=" font-['Architects_Daughter'] text-[11px] font-medium leading-5 tracking-tighter text-neutral-500"
+		class=" -translate-1 font-['Architects_Daughter'] text-[11px] font-medium leading-5 tracking-tighter text-neutral-500"
 	>
 		Upload Everywhere
 	</p>
