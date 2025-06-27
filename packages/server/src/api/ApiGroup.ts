3217d7c7bfa4ba40d04faf60edae155536211a61
Jackson Ernst - jaxer.eth <51183683+jaxernst@users.noreply.github.com>
2025-06-26 20:49:18 -0700

    yolo

--- packages/server/src/api/ApiGroup.ts
+++ packages/server/src/api/ApiGroup.ts
@@ -2,11 +2,9 @@ import { HttpApiBuilder } from "@effect/platform"
 import { FarMapApi } from "@farmap/domain/Api"
 import { Layer } from "effect"
 import { AuthApiLive } from "./AuthApiLive.js"
-import { CastActionApiLive } from "./CastActionApiLive.js"
 import { MapAttachmentsApiLive } from "./MapAttachmentApiLive.js"
 
 export const ApiLive = HttpApiBuilder.api(FarMapApi).pipe(
   Layer.provide(MapAttachmentsApiLive),
-  Layer.provide(AuthApiLive),
-  Layer.provide(CastActionApiLive)
+  Layer.provide(AuthApiLive)
 )
